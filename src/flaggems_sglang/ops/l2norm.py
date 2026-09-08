66fea2d1e1e4756d733f9aca0906ac912ce59bc8
liangkaiwen <liangkaiwen@bytedance.com>
2026-09-08 17:59:59 +0800

    chore(competition): T56 e3r2 final re-roll carrier

--- src/flaggems_sglang/ops/l2norm.py
+++ src/flaggems_sglang/ops/l2norm.py
@@ -1,4 +1,4 @@
-# r1 water re-roll carrier of e3 sub 11062 team-best bytes.
+# r2 final water re-roll carrier of e3 sub 11062 team-best bytes.
 # Copyright 2026 FlagOS Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
